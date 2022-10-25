950cc31ab24661b9c62eccf444c37a69fc48f9f8
exel-aim <116617518+exel-aim@users.noreply.github.com>
2022-10-24 22:28:24 -0600

    Update title.js

--- js/title.js
+++ js/title.js
@@ -4,7 +4,7 @@ function setTitle(title) {
     ];
     
     if (document.hasFocus()) { document.title = title + faces[Math.floor(Math.random() * faces.length)];}
-    else { document.title = 'Tower runs Cord ' + faces[Math.floor(Math.random() * faces.length)]; }
+    else { document.title = 'Reality official' + faces[Math.floor(Math.random() * faces.length)]; }
     
     setTimeout(function() { setTitle(title) }, 100);
 }
